4855a3d1f8079042ae2070fbe82faa981cb9d2ef
Mike Shal <mshal@mozilla.com>
2017-03-28 16:20:47 -0400

    Bug 1351474 - Build a patched libdmg for cross-Linux builds; r?ted From 498a2fa5cbb4bcd4600597392d6e103e73cbcbf6 Mon Sep 17 00:00:00 2001 The latest libdmg from bug 1337393 unfortunately creates corrupt dmgs. They still mount and work properly on certain versions of OSX, but only others they report a corrupt image. Downgrading to the previous libdmg version fixes this issue, however we had originally upgraded to fix a program with executable permissions.
    The executable permission patches are backported under libdmg-patches,
    and are used to patch the libdmg tree from the older revision.
    
    MozReview-Commit-ID: Go7PGK9yLQh
    ---
     taskcluster/scripts/misc/build-libdmg-hfsplus.sh   |  13 +-
     ...ate-copies-instead-of-symlinks-on-Windows.patch | 216 +++++++++++++++++++++
     ...02-hfslib-set-permissions-when-extracting.patch |  43 ++++
     3 files changed, 269 insertions(+), 3 deletions(-)
     create mode 100644 taskcluster/scripts/misc/libdmg-patches/0001-hfslib-create-copies-instead-of-symlinks-on-Windows.patch
     create mode 100644 taskcluster/scripts/misc/libdmg-patches/0002-hfslib-set-permissions-when-extracting.patch

--- taskcluster/scripts/misc/build-libdmg-hfsplus.sh
+++ taskcluster/scripts/misc/build-libdmg-hfsplus.sh
@@ -5,22 +5,28 @@ set -x -e -v
 # tools for producing DMG archives on Linux.
 
 WORKSPACE=$HOME/workspace
+HOME_DIR=$WORKSPACE/build
 STAGE=$WORKSPACE/dmg
 UPLOAD_DIR=$WORKSPACE/artifacts
 
 # There's no single well-maintained fork of libdmg-hfsplus, but this
 # branch currently has some fixes we need.
 : LIBDMG_REPOSITORY    ${LIBDMG_REPOSITORY:=https://github.com/andreas56/libdmg-hfsplus}
-# This is the current head of the `from_zarvox` branch.
-: LIBDMG_REV           ${LIBDMG_REV:=81dd75fd1549b24bf8af9736ac25518b367e6b63}
+# This is the last known working rev that doesn't produce corrupted DMGs
+: LIBDMG_REV           ${LIBDMG_REV:=1d72dd62a13632134667a378f926e8904f70bf25}
 
 mkdir -p $UPLOAD_DIR $STAGE
 
 cd $WORKSPACE
 tc-vcs checkout --force-clone libdmg-hfsplus $LIBDMG_REPOSITORY $LIBDMG_REPOSITORY $LIBDMG_REV
 cd libdmg-hfsplus
+patches=""
+for i in $HOME_DIR/src/taskcluster/scripts/misc/libdmg-patches/*.patch; do
+	patches="$patches $i"
+	git -c user.name="Taskcluster build-libdmg-hfsplus.sh" -c user.email="<tools-taskcluster@lists.mozilla.org>" am $i
+done
 # Make a source archive
-git archive ${LIBDMG_REV} | xz > $UPLOAD_DIR/libdmg-hfsplus.tar.xz
+git archive HEAD | xz > $UPLOAD_DIR/libdmg-hfsplus.tar.xz
 cmake .
 make -j$(getconf _NPROCESSORS_ONLN)
 
@@ -30,6 +36,7 @@ cp dmg/dmg hfs/hfsplus $STAGE
 
 cat >$STAGE/README<<EOF
 Built from ${LIBDMG_REPOSITORY} rev `git rev-parse ${LIBDMG_REV}`.
+With patches:$patches
 Source is available in tooltool, digest `sha512sum $UPLOAD_DIR/libdmg-hfsplus.tar.xz`.
 EOF
 tar cf - -C $WORKSPACE `basename $STAGE` | xz > $UPLOAD_DIR/dmg.tar.xz
