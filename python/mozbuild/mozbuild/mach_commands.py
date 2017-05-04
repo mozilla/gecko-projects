85a9a3c1146f6a8c4dffc1286472c2dd6191084c
Mike Shal <mshal@mozilla.com>
2017-05-04 18:52:34 -0400

    Bug 1360525 followup

--- python/mozbuild/mozbuild/mach_commands.py
+++ python/mozbuild/mozbuild/mach_commands.py
@@ -1937,6 +1937,6 @@ class Repackage(MachCommandBase):
         help='Mar binary path')
     @CommandArgument('--output', '-o', type=str, required=True,
         help='Output filename')
-    def repackage_installer(self, input, mar, output):
+    def repackage_mar(self, input, mar, output):
         from mozbuild.repackaging.mar import repackage_mar
         repackage_mar(self.topsrcdir, input, mar, output)
