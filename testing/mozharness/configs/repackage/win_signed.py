17880b90edee893727c9ce610eee2bf6bb2f035a
Aki Sasaki <asasaki@mozilla.com>
2017-05-22 15:01:54 -0700

    bug 1362489 - 2_win_mozconfig_mozharness_config
    MozReview-Commit-ID: 1neGUu5ueDl
    
    --HG--
    extra : rebase_source : ff3aee2d6187b8a113a5cbbdaece2a01a864bb12

---
+++ testing/mozharness/configs/repackage/win_signed.py
@@ -0,0 +1,39 @@
+import os
+import platform
+import sys
+
+if '64' in platform.machine():
+    plat = "win64"
+else:
+    plat = "win32"
+
+config = {
+    "input_home": "/home/worker/workspace/inputs",
+    "src_mozconfig": "browser/config/mozconfigs/win64/repack",
+
+    "download_config": {
+        "target.zip": os.environ.get("SIGNED_ZIP"),
+        "setup.exe": os.environ.get("SIGNED_SETUP"),
+        "setup-stub.exe": os.environ.get("SIGNED_SETUP_STUB"),
+        "mar.exe": os.environ.get("UNSIGNED_MAR"),
+    },
+
+    "repackage_config": [[
+        "installer", "--package", "target.zip", "--tag",
+        "browser/installer/windows/app.tag", "--setupexe", "setup.exe", "-o",
+        "target.installer.exe"
+    ], [
+        "installer", "--tag", "browser/installer/windows/stub.tag",
+         "--setupexe", "setup-stub.exe", "-o", "target.stub-installer.exe"
+    ], [
+        "mar", "-i", "target.zip", "--mar", "mar.exe", "-o",
+        "target.complete.mar"
+    ]],
+
+    # ToolTool
+    "tooltool_manifest_src": 'browser/config/tooltool-manifests/{}/releng.manifest'.format(plat),
+    "tooltool_url": 'http://relengapi/tooltool/',
+    "tooltool_bootstrap": "setup.sh",
+    'tooltool_script': [sys.executable, os.path.join(os.environ['MOZILLABUILD'], "/tooltool.py")],
+    'tooltool_cache': os.environ.get('TOOLTOOL_CACHE'),
+}
