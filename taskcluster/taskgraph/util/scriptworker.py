f6c8c528f398b9ad592aff8a136b43a3503ad0ef
Justin Wood <Callek@gmail.com>
2017-05-25 16:17:42 -0400

    Bug 1362489, fix beetmover scope deciding
    MozReview-Commit-ID: EKLdzsaXaMg

--- taskcluster/taskgraph/util/scriptworker.py
+++ taskcluster/taskgraph/util/scriptworker.py
@@ -103,6 +103,7 @@ BEETMOVER_SCOPE_ALIAS_TO_TARGET_TASK = [[
         'nightly_fennec',
         'nightly_linux',
         'nightly_macosx',
+        'nightly_win64',
         'mozilla_beta_tasks',
         'mozilla_release_tasks',
         'date_tasks',
