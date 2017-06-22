dd44f5a18beb28594c465d6bf471489ee0d6f83f
Johan Lorenzo <jlorenzo@mozilla.com>
2017-06-22 14:23:53 +0200

    Bug 1374589 - Port windows tests which require signed builds to in-tree tasks
    MozReview-Commit-ID: I9fUdxdRGmD
    
    --HG--
    extra : transplant_source : E%AAh%DD%FF%A2m%F1%93%F9%DD%E9%1A%9C%C4%9B%F2%A5H%B5

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -251,6 +251,9 @@ test_description_schema = Schema({
             # chunking-args = test-suite-suffix; "<CHUNK>" in this string will
             # be replaced with the chunk number.
             Optional('chunk-suffix'): basestring,
+
+            Required('requires-signed-builds', default=False): optionally_keyed_by(
+                'test-platform', bool),
         }
     ),
 
@@ -287,6 +290,8 @@ test_description_schema = Schema({
     Optional('when'): Any({
         Optional('files-changed'): [basestring],
     }),
+
+    Optional('build-signing-ci-label'): basestring,
 
 }, required=True)
 
@@ -389,6 +394,7 @@ def set_target(config, tests):
         else:
             target = 'target.tar.bz2'
         test['mozharness']['build-artifact-name'] = 'public/build/' + target
+
         yield test
 
 
@@ -494,6 +500,7 @@ def handle_keyed_by(config, tests):
         'mozharness.chunked',
         'mozharness.config',
         'mozharness.extra-options',
+        'mozharness.requires-signed-builds',
     ]
     for test in tests:
         for field in fields:
@@ -791,6 +798,11 @@ def make_job_description(config, tests):
         jobdesc['when'] = test.get('when', {})
         jobdesc['attributes'] = attributes
         jobdesc['dependencies'] = {'build': build_label}
+
+        if test['mozharness']['requires-signed-builds'] is True:
+            print(test)
+            jobdesc['dependencies']['build-signing-ci'] = test['build-signing-ci-label']
+
         jobdesc['expires-after'] = test['expires-after']
         jobdesc['routes'] = []
         jobdesc['run-on-projects'] = test['run-on-projects']
