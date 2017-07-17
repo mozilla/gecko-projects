6998cff9320ea561c0113b7010bd2cc3de5c135e
Chris AtLee <catlee@mozilla.com>
2017-07-17 15:57:38 -0400

    Bug 1379661: Fix win8 typo

--- taskcluster/taskgraph/transforms/job/mozharness_test.py
+++ taskcluster/taskgraph/transforms/job/mozharness_test.py
@@ -40,7 +40,7 @@ BUILDER_NAME_PREFIX = {
     'windows7-32': 'Windows 7 32-bit',
     'windows7-32-nightly': 'Windows 7 32-bit',
     'windows7-32-pgo': 'Windows 7 32-bit',
-    'windows8-64': 'Windows 8 32-bit',
+    'windows8-64': 'Windows 8 64-bit',
     'windows8-64-nightly': 'Windows 8 64-bit',
     'windows8-64-pgo': 'Windows 8 64-bit',
 }
