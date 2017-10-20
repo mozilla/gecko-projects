d4e7426ff64670d65a57822fd9851a26032c11c5
Aki Sasaki <asasaki@mozilla.com>
2017-10-19 21:53:57 -0700

    reenable dep android builds

--- taskcluster/taskgraph/target_tasks.py
+++ taskcluster/taskgraph/target_tasks.py
@@ -60,7 +60,9 @@ def filter_beta_release_tasks(task, parameters, ignore_kinds=None, allow_l10n=Fa
             # On beta, Nightly builds are already PGOs
             'linux-pgo', 'linux64-pgo',
             'win32-pgo', 'win64-pgo',
-            ) or str(platform).startswith('android'):
+            )
+        return False
+    if str(platform).startswith('android') and 'nightly' in str(platform):
         return False
 
     if platform in (
