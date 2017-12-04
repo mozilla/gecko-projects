77671dd049a68aa55331fa0bf05bed558032d557
Ben Hearsum <bhearsum@mozilla.com>
2017-12-04 13:22:27 -0500

    Fix mozharness config to use the correct username.

--- testing/mozharness/configs/releases/dev_updates_firefox_beta.py
+++ testing/mozharness/configs/releases/dev_updates_firefox_beta.py
@@ -21,7 +21,7 @@ config = {
     "previous_archive_prefix": "https://archive.mozilla.org/pub",
     "download_domain": "download.mozilla.org",
     "balrog_url": "https://aus4.stage.mozaws.net/",
-    "balrog_username": "balrog-stage-ffxbld.com",
+    "balrog_username": "balrog-stage-ffxbld",
     "update_channels": {
         "beta": {
             "version_regex": r"^(\d+\.\d+(b\d+)?)$",
