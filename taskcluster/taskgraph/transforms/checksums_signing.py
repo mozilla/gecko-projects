78094f4f358d0ff60a66da2a4378dd0f80173b32
Aki Sasaki <asasaki@mozilla.com>
2017-12-06 18:45:51 -0800

    STAGING ONLY fix flake8 DONTBUILD
    MozReview-Commit-ID: KsM2Ao1mHJl
    
    --HG--
    extra : histedit_source : 914852755dbfc563ebe55946a8f90e499c8d63df%2C419fadf77b031627009ead27d88e64b477d59c91

--- taskcluster/taskgraph/transforms/checksums_signing.py
+++ taskcluster/taskgraph/transforms/checksums_signing.py
@@ -10,7 +10,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 from taskgraph.transforms.base import TransformSequence
 from taskgraph.util.attributes import copy_attributes_from_dependent_job
 from taskgraph.util.schema import validate_schema, Schema
-from taskgraph.util.scriptworker import get_signing_cert_scope
+# from taskgraph.util.scriptworker import get_signing_cert_scope
 from taskgraph.transforms.task import task_description_schema
 from voluptuous import Any, Required, Optional
 
@@ -84,7 +84,7 @@ def make_checksums_signing_description(config, jobs):
             "formats": ["gpg"]
         }]
 
-        signing_cert_scope = get_signing_cert_scope(config)
+        # signing_cert_scope = get_signing_cert_scope(config)
         task = {
             'label': label,
             'description': description,
