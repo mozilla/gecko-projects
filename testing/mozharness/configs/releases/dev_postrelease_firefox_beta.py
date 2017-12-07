2e0c58d0b975bd05d4a02ae55efc3c660c5617f5
Mihai Tabara <mtabara@mozilla.com>
2017-12-08 00:02:15 +0200

    Fix version bump moz configs by s,jamun,maple.
    MozReview-Commit-ID: PNyWEKWdW9

--- testing/mozharness/configs/releases/dev_postrelease_firefox_beta.py
+++ testing/mozharness/configs/releases/dev_postrelease_firefox_beta.py
@@ -4,14 +4,14 @@ config = {
     "version_files": [{"file": "browser/config/version_display.txt"}],
     "repo": {
         # maple is used for staging mozilla-beta
-        "repo": "https://hg.mozilla.org/projects/jamun",
+        "repo": "https://hg.mozilla.org/projects/maple",
         "branch": "default",
-        "dest": "jamun",
+        "dest": "maple",
         "vcs": "hg",
         "clone_upstream_url": "https://hg.mozilla.org/mozilla-unified",
     },
     # date is used for staging mozilla-beta
-    "push_dest": "ssh://hg.mozilla.org/projects/jamun",
+    "push_dest": "ssh://hg.mozilla.org/projects/maple",
     "ignore_no_changes": True,
     "ssh_user": "ffxbld",
     "ssh_key": "~/.ssh/ffxbld_rsa",
