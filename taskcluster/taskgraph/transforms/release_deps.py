d4999023adb74bf6d689a7cbe97a173941f03b06
Aki Sasaki <asasaki@mozilla.com>
2017-12-11 16:00:42 -0800

    release_deps: remove erroneous comment, fix double if
    MozReview-Commit-ID: L3xqJKtB1q9
    
    --HG--
    extra : rebase_source : dc3016d5e6d8e73d94170e61dd0045adecbc0879

--- taskcluster/taskgraph/transforms/release_deps.py
+++ taskcluster/taskgraph/transforms/release_deps.py
@@ -60,11 +60,8 @@ def add_dependencies(config, jobs):
             if dep_phase and PHASES.index(dep_phase) > PHASES.index(phase):
                 continue
             # Add matching product tasks to deps
-            if _get_product(dep_task.task) == product:
-                dependencies[dep_task.label] = dep_task.label
-            # this is dumb, but i don't know how to get beetmover to set
-            # shipping-product in a way that _get_product recognizes
-            if dep_task.attributes.get('shipping_product') == product:
+            if _get_product(dep_task.task) == product or \
+                    dep_task.attributes.get('shipping_product') == product:
                 dependencies[dep_task.label] = dep_task.label
 
         job.setdefault('dependencies', {}).update(dependencies)
