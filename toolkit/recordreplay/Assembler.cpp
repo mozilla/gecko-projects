be9ed083bdff52f8c31db6de8afa8f617a0d7d78
Brian Hackett <bhackett1024@gmail.com>
2018-02-23 06:16:57 -0700

    Bug 1207696 Part 1h - Redirections infrastructure, r=froydnj.

---
+++ toolkit/recordreplay/Assembler.cpp
@@ -0,0 +1,399 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "Assembler.h"
+
+#include "ProcessRecordReplay.h"
+#include "udis86/types.h"
+
+#ifdef XP_MACOSX
+#include <sys/mman.h>
+#endif
+
+namespace mozilla {
+namespace recordreplay {
+
+Assembler::Assembler()
+  : mCursor(nullptr)
+  , mCursorEnd(nullptr)
+  , mCanAllocateStorage(true)
+  , mIpStart(nullptr)
+  , mIpEnd(nullptr)
+{}
+
+Assembler::Assembler(uint8_t* aStorage, size_t aSize)
+  : mCursor(aStorage)
+  , mCursorEnd(aStorage + aSize)
+  , mCanAllocateStorage(false)
+  , mIpStart(nullptr)
+  , mIpEnd(nullptr)
+{}
+
+Assembler::~Assembler()
+{
+  MOZ_ASSERT(!mIpStart);
+  MOZ_ASSERT(!mIpEnd);
+  MOZ_ASSERT(mCopiedInstructions.empty());
+  MOZ_ASSERT(mInternalJumps.empty());
+}
+
+void
+Assembler::PrepareToCopyInstructions(uint8_t* aIp, uint8_t* aIpEnd)
+{
+  MOZ_ASSERT(!mIpStart);
+  MOZ_ASSERT(!mIpEnd);
+  MOZ_ASSERT(mCopiedInstructions.empty());
+  MOZ_ASSERT(mInternalJumps.empty());
+
+  mIpStart = aIp;
+  mIpEnd = aIpEnd;
+}
+
+void
+Assembler::NoteOriginalInstruction(uint8_t* aIp)
+{
+  if (!mIpStart) {
+    return;
+  }
+  MOZ_ASSERT(aIp >= mIpStart);
+  MOZ_ASSERT(aIp < mIpEnd);
+
+  mCopiedInstructions.emplaceBack(aIp, Current());
+}
+
+void
+Assembler::FinishCopyingInstructions()
+{
+  MOZ_ASSERT(mIpStart);
+  MOZ_ASSERT(mIpEnd);
+
+  // Patch each internal jump to the point where the jump's target was copied.
+  for (auto pair : mInternalJumps) {
+    uint8_t* source = pair.first;
+    uint8_t* target = pair.second;
+
+    MOZ_ASSERT(MemoryContains(mIpStart, mIpEnd - mIpStart, target));
+    uint8_t* newTarget = nullptr;
+    for (auto copyPair : mCopiedInstructions) {
+      if (copyPair.first == target) {
+	newTarget = copyPair.second;
+	break;
+      }
+    }
+    MOZ_RELEASE_ASSERT(newTarget);
+
+    PatchJump(source, newTarget);
+  }
+
+  mCopiedInstructions.clear();
+  mInternalJumps.clear();
+
+  mIpStart = nullptr;
+  mIpEnd = nullptr;
+}
+
+void
+Assembler::Advance(size_t aSize)
+{
+  MOZ_RELEASE_ASSERT(aSize <= MaximumAdvance);
+  mCursor += aSize;
+}
+
+static const size_t JumpBytes = 17;
+
+uint8_t*
+Assembler::Current()
+{
+  // Reallocate the buffer if there is not enough space. We need enough for the
+  // maximum space used by any of the assembling functions, as well as for a
+  // following jump for fallthrough to the next allocated space.
+  if (size_t(mCursorEnd - mCursor) <= MaximumAdvance + JumpBytes) {
+    MOZ_RELEASE_ASSERT(mCanAllocateStorage);
+
+    // Allocate some writable, executable memory.
+    static const size_t BufferSize = PageSize;
+    uint8_t* buffer = new uint8_t[PageSize];
+    UnprotectExecutableMemory(buffer, PageSize);
+
+    if (mCursor) {
+      // Patch a jump for fallthrough from the last allocation.
+      MOZ_RELEASE_ASSERT(size_t(mCursorEnd - mCursor) >= JumpBytes);
+      PatchJump(mCursor, buffer);
+    }
+
+    mCursor = buffer;
+    mCursorEnd = &buffer[BufferSize];
+  }
+
+  return mCursor;
+}
+
+static void
+Push16(uint8_t** aIp, uint16_t aValue)
+{
+  (*aIp)[0] = 0x66;
+  (*aIp)[1] = 0x68;
+  *reinterpret_cast<uint16_t*>(*aIp + 2) = aValue;
+  (*aIp) += 4;
+}
+
+/* static */ void
+Assembler::PatchJump(uint8_t* aIp, void* aTarget)
+{
+  // Push the target literal onto the stack, 2 bytes at a time. This is
+  // apparently the best way of getting an arbitrary 8 byte literal onto the
+  // stack, as 4 byte literals we push will be sign extended to 8 bytes.
+  size_t ntarget = reinterpret_cast<size_t>(aTarget);
+  Push16(&aIp, ntarget >> 48);
+  Push16(&aIp, ntarget >> 32);
+  Push16(&aIp, ntarget >> 16);
+  Push16(&aIp, ntarget);
+  *aIp = 0xC3; // ret
+}
+
+void
+Assembler::Jump(void* aTarget)
+{
+  if (mIpStart && MemoryContains(mIpStart, mIpEnd - mIpStart, aTarget)) {
+    mInternalJumps.emplaceBack(Current(), (uint8_t*) aTarget);
+  } else {
+    PatchJump(Current(), aTarget);
+  }
+  Advance(JumpBytes);
+}
+
+static uint8_t
+OppositeJump(uint8_t aOpcode)
+{
+  // Get the opposite for a single byte conditional jump opcode. Opposite
+  // opcodes are adjacent, e.g. 0x7C -> jl and 0x7D -> jge.
+  MOZ_RELEASE_ASSERT(aOpcode >= 0x70 && aOpcode <= 0x7F);
+  return (aOpcode & 1) ? aOpcode - 1 : aOpcode + 1;
+}
+
+void
+Assembler::ConditionalJump(uint8_t aCode, void* aTarget)
+{
+  uint8_t* ip = Current();
+  ip[0] = OppositeJump(aCode);
+  ip[1] = (uint8_t) JumpBytes;
+  PatchJump(ip + 2, aTarget);
+  Advance(2 + JumpBytes);
+}
+
+void
+Assembler::CopyInstruction(uint8_t* aIp, size_t aSize)
+{
+  MOZ_RELEASE_ASSERT(aSize <= MaximumInstructionLength);
+  memcpy(Current(), aIp, aSize);
+  Advance(aSize);
+}
+
+void
+Assembler::PushRax()
+{
+  NewInstruction(0x50);
+}
+
+void
+Assembler::PopRax()
+{
+  NewInstruction(0x58);
+}
+
+void
+Assembler::JumpToRax()
+{
+  NewInstruction(0xFF, 0xE0);
+}
+
+void
+Assembler::CallRax()
+{
+  NewInstruction(0xFF, 0xD0);
+}
+
+void
+Assembler::LoadRax(size_t aWidth)
+{
+  switch (aWidth) {
+  case 1: NewInstruction(0x8A, 0x00); break;
+  case 2: NewInstruction(0x66, 0x8B, 0x00); break;
+  case 4: NewInstruction(0x8B, 0x00); break;
+  case 8: NewInstruction(0x48, 0x8B, 0x00); break;
+  default: MOZ_CRASH();
+  }
+}
+
+void
+Assembler::CompareRaxWithTopOfStack()
+{
+  NewInstruction(0x48, 0x39, 0x04, 0x24);
+}
+
+void
+Assembler::PushRbx()
+{
+  NewInstruction(0x53);
+}
+
+void
+Assembler::PopRbx()
+{
+  NewInstruction(0x5B);
+}
+
+void
+Assembler::StoreRbxToRax(size_t aWidth)
+{
+  switch (aWidth) {
+  case 1: NewInstruction(0x88, 0x18); break;
+  case 2: NewInstruction(0x66, 0x89, 0x18); break;
+  case 4: NewInstruction(0x89, 0x18); break;
+  case 8: NewInstruction(0x48, 0x89, 0x18); break;
+  default: MOZ_CRASH();
+  }
+}
+
+void
+Assembler::CompareValueWithRax(uint8_t aValue, size_t aWidth)
+{
+  switch (aWidth) {
+  case 1: NewInstruction(0x3C, aValue); break;
+  case 2: NewInstruction(0x66, 0x83, 0xF8, aValue); break;
+  case 4: NewInstruction(0x83, 0xF8, aValue); break;
+  case 8: NewInstruction(0x48, 0x83, 0xF8, aValue); break;
+  default: MOZ_CRASH();
+  }
+}
+
+static const size_t MoveImmediateBytes = 10;
+
+/* static */ void
+Assembler::PatchMoveImmediateToRax(uint8_t* aIp, void* aValue)
+{
+  aIp[0] = 0x40 | (1 << 3);
+  aIp[1] = 0xB8;
+  *reinterpret_cast<void**>(aIp + 2) = aValue;
+}
+
+void
+Assembler::MoveImmediateToRax(void* aValue)
+{
+  MOZ_RELEASE_ASSERT(!mIpStart || (aValue < mIpStart) || (aValue >= mIpEnd));
+  PatchMoveImmediateToRax(Current(), aValue);
+  Advance(MoveImmediateBytes);
+}
+
+void
+Assembler::MoveRaxToRegister(/*ud_type*/ int aRegister)
+{
+  MOZ_RELEASE_ASSERT(aRegister == NormalizeRegister(aRegister));
+
+  uint8_t* ip = Current();
+  if (aRegister <= UD_R_RDI) {
+    ip[0] = 0x48;
+    ip[1] = 0x89;
+    ip[2] = 0xC0 + aRegister - UD_R_RAX;
+  } else {
+    ip[0] = 0x49;
+    ip[1] = 0x89;
+    ip[2] = 0xC0 + aRegister - UD_R_R8;
+  }
+  Advance(3);
+}
+
+void
+Assembler::MoveRegisterToRax(/*ud_type*/ int aRegister)
+{
+  MOZ_RELEASE_ASSERT(aRegister == NormalizeRegister(aRegister));
+
+  uint8_t* ip = Current();
+  if (aRegister <= UD_R_RDI) {
+    ip[0] = 0x48;
+    ip[1] = 0x89;
+    ip[2] = 0xC0 + (aRegister - UD_R_RAX) * 8;
+  } else {
+    ip[0] = 0x4C;
+    ip[1] = 0x89;
+    ip[2] = 0xC0 + (aRegister - UD_R_R8) * 8;
+  }
+  Advance(3);
+}
+
+/* static */ /*ud_type*/ int
+Assembler::NormalizeRegister(/*ud_type*/ int aRegister)
+{
+  if (aRegister >= UD_R_AL && aRegister <= UD_R_R15B) {
+    return aRegister - UD_R_AL + UD_R_RAX;
+  }
+  if (aRegister >= UD_R_AX && aRegister <= UD_R_R15W) {
+    return aRegister - UD_R_AX + UD_R_RAX;
+  }
+  if (aRegister >= UD_R_EAX && aRegister <= UD_R_R15D) {
+    return aRegister - UD_R_EAX + UD_R_RAX;
+  }
+  if (aRegister >= UD_R_RAX && aRegister <= UD_R_R15) {
+    return aRegister;
+  }
+  return UD_NONE;
+}
+
+/* static */ bool
+Assembler::CanPatchShortJump(uint8_t* aIp, void* aTarget)
+{
+  return (aIp + 2 - 128 <= aTarget) && (aIp + 2 + 127 >= aTarget);
+}
+
+/* static */ void
+Assembler::PatchShortJump(uint8_t* aIp, void* aTarget)
+{
+  MOZ_RELEASE_ASSERT(CanPatchShortJump(aIp, aTarget));
+  aIp[0] = 0xEB;
+  aIp[1] = uint8_t(static_cast<uint8_t*>(aTarget) - aIp - 2);
+}
+
+/* static */ void
+Assembler::PatchJumpClobberRax(uint8_t* aIp, void* aTarget)
+{
+  PatchMoveImmediateToRax(aIp, aTarget);
+  aIp[10] = 0x50; // push %rax
+  aIp[11] = 0xC3; // ret
+}
+
+/* static */ void
+Assembler::PatchClobber(uint8_t* aIp)
+{
+  aIp[0] = 0xCC; // int3
+}
+
+static uint8_t*
+PageStart(uint8_t* aPtr)
+{
+  static_assert(sizeof(size_t) == sizeof(uintptr_t), "Unsupported Platform");
+  return reinterpret_cast<uint8_t*>(reinterpret_cast<size_t>(aPtr) & ~(PageSize - 1));
+}
+
+void
+UnprotectExecutableMemory(uint8_t* aAddress, size_t aSize)
+{
+  MOZ_ASSERT(aSize);
+  uint8_t* pageStart = PageStart(aAddress);
+  uint8_t* pageEnd = PageStart(aAddress + aSize - 1) + PageSize;
+#if defined(XP_MACOSX)
+  int ret = mprotect(pageStart, pageEnd - pageStart, PROT_READ | PROT_EXEC | PROT_WRITE);
+  MOZ_RELEASE_ASSERT(ret >= 0);
+#elif defined(WIN32)
+  DWORD oldProtect;
+  BOOL res = VirtualProtect(pageStart, pageEnd - pageStart, PAGE_EXECUTE_READWRITE, &oldProtect);
+  MOZ_RELEASE_ASSERT(res);
+#else
+  #error "Unknown platform"
+#endif
+}
+
+} // namespace recordreplay
+} // namespace mozilla
---
+++ toolkit/recordreplay/Assembler.h
@@ -0,0 +1,192 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_toolkit_recordreplay_Assembler_h
+#define mozilla_toolkit_recordreplay_Assembler_h
+
+#include "InfallibleVector.h"
+
+#include <utility>
+
+namespace mozilla {
+namespace recordreplay {
+
+// Assembler for x64 instructions. This is a simple assembler that is primarily
+// designed for use in copying instructions from a function that is being
+// redirected.
+class Assembler
+{
+public:
+  // Create an assembler that allocates its own instruction storage. Assembled
+  // code will never be reclaimed by the system.
+  Assembler();
+
+  // Create an assembler that uses the specified memory range for instruction
+  // storage.
+  Assembler(uint8_t* aStorage, size_t aSize);
+
+  ~Assembler();
+
+  // Specify an original range of instructions which we are about to copy.
+  // This must be followed by a call to FinishCopyingInstructions.
+  void PrepareToCopyInstructions(uint8_t* aIp, uint8_t* aIpEnd);
+
+  // Mark the point at which we start copying an instruction in the original
+  // range.
+  void NoteOriginalInstruction(uint8_t* aIp);
+
+  // Mark the point where we are done copying the range of instructions.
+  void FinishCopyingInstructions();
+
+  // Get the address where the next assembled instruction will be placed.
+  uint8_t* Current();
+
+///////////////////////////////////////////////////////////////////////////////
+// Routines for assembling instructions in new instruction storage
+///////////////////////////////////////////////////////////////////////////////
+
+  // Jump to aTarget. If aTarget is in the range of instructions being copied,
+  // the target will be the copy of aTarget instead.
+  void Jump(void* aTarget);
+
+  // Conditionally jump to aTarget, depending on the short jump opcode aCode.
+  // If aTarget is in the range of instructions being copied, the target will
+  // be the copy of aTarget instead.
+  void ConditionalJump(uint8_t aCode, void* aTarget);
+
+  // Copy an instruction verbatim from aIp.
+  void CopyInstruction(uint8_t* aIp, size_t aSize);
+
+  // push/pop %rax
+  void PushRax();
+  void PopRax();
+
+  // jump *%rax
+  void JumpToRax();
+
+  // call *%rax
+  void CallRax();
+
+  // movq/movl/movb 0(%rax), %rax
+  void LoadRax(size_t aWidth);
+
+  // cmpq %rax, 0(%rsp)
+  void CompareRaxWithTopOfStack();
+
+  // push/pop %rbx
+  void PushRbx();
+  void PopRbx();
+
+  // movq/movl/movb %rbx, 0(%rax)
+  void StoreRbxToRax(size_t aWidth);
+
+  // cmpq/cmpb $literal8, %rax
+  void CompareValueWithRax(uint8_t aValue, size_t aWidth);
+
+  // movq $value, %rax
+  void MoveImmediateToRax(void* aValue);
+
+  // movq %rax, register
+  void MoveRaxToRegister(/*ud_type*/ int aRegister);
+
+  // movq register, %rax
+  void MoveRegisterToRax(/*ud_type*/ int aRegister);
+
+  // Normalize a Udis86 register to its 8 byte version, returning UD_NONE/zero
+  // for unexpected registers.
+  static /*ud_type*/ int NormalizeRegister(/*ud_type*/ int aRegister);
+
+///////////////////////////////////////////////////////////////////////////////
+// Routines for assembling instructions at arbitrary locations
+///////////////////////////////////////////////////////////////////////////////
+
+  // Return whether it is possible to patch a short jump to aTarget from aIp.
+  static bool CanPatchShortJump(uint8_t* aIp, void* aTarget);
+
+  // Patch a short jump to aTarget at aIp.
+  static void PatchShortJump(uint8_t* aIp, void* aTarget);
+
+  // Patch a long jump to aTarget at aIp. Rax may be clobbered.
+  static void PatchJumpClobberRax(uint8_t* aIp, void* aTarget);
+
+  // Patch the value used in an earlier MoveImmediateToRax call.
+  static void PatchMoveImmediateToRax(uint8_t* aIp, void* aValue);
+
+  // Patch an int3 breakpoint instruction at Ip.
+  static void PatchClobber(uint8_t* aIp);
+
+private:
+  // Patch a jump that doesn't clobber any instructions.
+  static void PatchJump(uint8_t* aIp, void* aTarget);
+
+  // Consume some instruction storage.
+  void Advance(size_t aSize);
+
+  // The maximum amount we can write at a time without a jump potentially
+  // being introduced into the instruction stream.
+  static const size_t MaximumAdvance = 20;
+
+  inline size_t CountBytes() { return 0; }
+
+  template <typename... Tail>
+  inline size_t CountBytes(uint8_t aByte, Tail... aMoreBytes) {
+    return 1 + CountBytes(aMoreBytes...);
+  }
+
+  inline void CopyBytes(uint8_t* aIp) {}
+
+  template <typename... Tail>
+  inline void CopyBytes(uint8_t* aIp, uint8_t aByte, Tail... aMoreBytes) {
+    *aIp = aByte;
+    CopyBytes(aIp + 1, aMoreBytes...);
+  }
+
+  // Write a complete instruction with bytes specified as parameters.
+  template <typename... ByteList>
+  inline void NewInstruction(ByteList... aBytes) {
+    size_t numBytes = CountBytes(aBytes...);
+    MOZ_ASSERT(numBytes <= MaximumAdvance);
+    uint8_t* ip = Current();
+    CopyBytes(ip, aBytes...);
+    Advance(numBytes);
+  }
+
+  // Storage for assembling new instructions.
+  uint8_t* mCursor;
+  uint8_t* mCursorEnd;
+  bool mCanAllocateStorage;
+
+  // Optional range of instructions being copied.
+  uint8_t* mIpStart;
+  uint8_t* mIpEnd;
+
+  // For instructions in [mIpStart, mIpEnd] which we have copied or started
+  // copying, association between the instruction original and copy pointers.
+  InfallibleVector<std::pair<uint8_t*,uint8_t*>> mCopiedInstructions;
+
+  // For jumps we have encountered into [mIpStart, mIpEnd], association between
+  // the source (in generated code) and target (in the above range) of the
+  // jump. These will be updated to refer to their copy in generated code by
+  // FinishCopyingInstructions.
+  InfallibleVector<std::pair<uint8_t*,uint8_t*>> mInternalJumps;
+};
+
+// The number of instruction bytes required for a short jump.
+static const size_t ShortJumpBytes = 2;
+
+// The number of instruction bytes required for a jump that may clobber rax.
+static const size_t JumpBytesClobberRax = 12;
+
+// The maximum byte length of an x86/x64 instruction.
+static const size_t MaximumInstructionLength = 15;
+
+// Make a region of memory RWX.
+void UnprotectExecutableMemory(uint8_t* aAddress, size_t aSize);
+
+} // recordreplay
+} // mozilla
+
+#endif // mozilla_toolkit_recordreplay_Assembler_h
