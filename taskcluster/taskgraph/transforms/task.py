db505ff08f6b1990600b55f835297b86f8bddbba
Johan Lorenzo <jlorenzo@mozilla.com>
2018-02-26 19:12:48 +0100

    Fix bad merge

--- taskcluster/taskgraph/transforms/task.py
+++ taskcluster/taskgraph/transforms/task.py
@@ -23,7 +23,12 @@ from taskgraph.util.hash import hash_path
 from taskgraph.util.treeherder import split_symbol
 from taskgraph.transforms.base import TransformSequence
 from taskgraph.util.schema import validate_schema, Schema, optionally_keyed_by, resolve_keyed_by
-from taskgraph.util.scriptworker import get_release_config
+from taskgraph.util.scriptworker import (
+    BALROG_ACTIONS,
+    get_balrog_action_scope,
+    get_balrog_server_scope,
+    get_release_config,
+)
 from voluptuous import Any, Required, Optional, Extra
 from taskgraph import GECKO, MAX_DEPENDENCIES
 from ..util import docker as dockerutil
@@ -513,6 +518,15 @@ task_description_schema = Schema({
         # locale key, if this is a locale beetmover job
         Optional('locale'): basestring,
 
+        Required('release-properties'): {
+            'app-name': basestring,
+            'app-version': basestring,
+            'branch': basestring,
+            'build-id': basestring,
+            'hash-type': basestring,
+            'platform': basestring,
+        },
+
         # list of artifact URLs for the artifacts that should be beetmoved
         Required('upstream-artifacts'): [{
             # taskId of the task with the artifact
@@ -535,9 +549,18 @@ task_description_schema = Schema({
         Required('product'): basestring,
     }, {
         Required('implementation'): 'balrog',
+        Required('balrog-action'): Any(*BALROG_ACTIONS),
+        Optional('product'): basestring,
+        Optional('platforms'): [basestring],
+        Optional('channel-names'): optionally_keyed_by('project', [basestring]),
+        Optional('require-mirrors'): bool,
+        Optional('publish-rules'): optionally_keyed_by('project', [int]),
+        Optional('rules-to-update'): optionally_keyed_by('project', [basestring]),
+        Optional('archive-domain'): optionally_keyed_by('project', basestring),
+        Optional('download-domain'): optionally_keyed_by('project', basestring),
 
         # list of artifact URLs for the artifacts that should be beetmoved
-        Required('upstream-artifacts'): [{
+        Optional('upstream-artifacts'): [{
             # taskId of the task with the artifact
             Required('taskId'): taskref_or_string,
 
@@ -592,6 +615,13 @@ task_description_schema = Schema({
     }, {
         Required('implementation'): 'shipit',
         Required('release-name'): basestring,
+    }, {
+        Required('implementation'): 'treescript',
+        Required('tag'): bool,
+        Required('bump'): bool,
+        Optional('bump-files'): [basestring],
+        Required('force-dry-run', default=True): bool,
+        Required('push', default=False): bool
     }),
 })
 
@@ -1022,11 +1052,20 @@ def build_binary_transparency_payload(config, task, task_def):
 def build_beetmover_payload(config, task, task_def):
     worker = task['worker']
     release_config = get_release_config(config)
+    release_properties = worker['release-properties']
 
     task_def['payload'] = {
         'maxRunTime': worker['max-run-time'],
+        'releaseProperties': {
+            'appName': release_properties['app-name'],
+            'appVersion': release_properties['app-version'],
+            'branch': release_properties['branch'],
+            'buildid': release_properties['build-id'],
+            'hashType': release_properties['hash-type'],
+            'platform': release_properties['platform'],
+        },
         'upload_date': config.params['build_date'],
-        'upstreamArtifacts':  worker['upstream-artifacts']
+        'upstreamArtifacts':  worker['upstream-artifacts'],
     }
     if worker.get('locale'):
         task_def['payload']['locale'] = worker['locale']
@@ -1050,10 +1089,45 @@ def build_beetmover_cdns_payload(config, task, task_def):
 @payload_builder('balrog')
 def build_balrog_payload(config, task, task_def):
     worker = task['worker']
+    release_config = get_release_config(config)
 
-    task_def['payload'] = {
-        'upstreamArtifacts':  worker['upstream-artifacts']
-    }
+    server_scope = get_balrog_server_scope(config)
+    action_scope = get_balrog_action_scope(config, action=worker['balrog-action'])
+    task_def['scopes'] = [server_scope, action_scope]
+
+    if worker['balrog-action'] == 'submit-locale':
+        task_def['payload'] = {
+            'upstreamArtifacts':  worker['upstream-artifacts']
+        }
+    else:
+        for prop in ('archive-domain', 'channel-names', 'download-domain',
+                     'publish-rules', 'rules-to-update'):
+            if prop in worker:
+                resolve_keyed_by(
+                    worker, prop, task['description'],
+                    **config.params
+                )
+        task_def['payload'] = {
+            'build_number': release_config['build_number'],
+            'product': worker['product'],
+            'version': release_config['version'],
+        }
+        if worker['balrog-action'] == 'submit-toplevel':
+            task_def['payload'].update({
+                'app_version': release_config['appVersion'],
+                'archive_domain': worker['archive-domain'],
+                'channel_names': worker['channel-names'],
+                'download_domain': worker['download-domain'],
+                'partial_versions': release_config.get('partial_versions', ""),
+                'platforms': worker['platforms'],
+                'rules_to_update': worker['rules-to-update'],
+                'require_mirrors': worker['require-mirrors'],
+            })
+        else:  # schedule / ship
+            task_def['payload'].update({
+                'publish_rules': worker['publish-rules'],
+                'release_eta': config.params.get('release_eta') or '',
+            })
 
 
 @payload_builder('bouncer-aliases')
@@ -1101,6 +1175,45 @@ def build_ship_it_payload(config, task, task_def):
     task_def['payload'] = {
         'release_name': worker['release-name']
     }
+
+
+@payload_builder('treescript')
+def build_treescript_payload(config, task, task_def):
+    worker = task['worker']
+    release_config = get_release_config(config)
+
+    task_def['payload'] = {}
+    task_def.setdefault('scopes', [])
+    if worker['tag']:
+        product = task['shipping-product'].upper()
+        version = release_config['version'].replace('.', '_')
+        buildnum = release_config['build_number']
+        tag_names = [
+            "{}_{}_BUILD{}".format(product, version, buildnum),
+            "{}_{}_RELEASE".format(product, version)
+        ]
+        tag_info = {
+            'tags': tag_names,
+            'revision': config.params['head_rev']
+        }
+        task_def['payload']['tag_info'] = tag_info
+        task_def['scopes'].append('project:releng:treescript:action:tagging')
+
+    if worker['bump']:
+        if not worker['bump-files']:
+            raise Exception("Version Bump requested without bump-files")
+
+        bump_info = {}
+        bump_info['next_version'] = release_config['next_version']
+        bump_info['files'] = worker['bump-files']
+        task_def['payload']['version_bump_info'] = bump_info
+        task_def['scopes'].append('project:releng:treescript:action:version_bump')
+
+    if worker['push']:
+        task_def['scopes'].append('project:releng:treescript:action:push')
+
+    if worker['force-dry-run']:
+        task_def['payload']['dry_run'] = True
 
 
 @payload_builder('invalid')
