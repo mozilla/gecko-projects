46f19853a872b1e71417bf3fad373fa7d333efa3
Ben Hearsum <bhearsum@mozilla.com>
2018-03-07 14:53:45 -0500

    Stub out artifacts properly for all platforms and use new mozharness configs for repackage.

--- taskcluster/taskgraph/transforms/partner_repack.py
+++ taskcluster/taskgraph/transforms/partner_repack.py
@@ -40,11 +40,46 @@ def add_command(config, tasks):
         if not build_task:
             raise Exception("Couldn't find build task")
 
-        task["run"]["command"] = " ".join([
-            "cd", "/builds/worker/checkouts/gecko", "&&",
-            "curl -L https://queue.taskcluster.net/v1/task/YfqwST9zRo2-QddTuetDQA/runs/0/artifacts/public/build/target.dmg > partner1.dmg && "
-            "cp partner1.dmg partner2.dmg && "
-            "cp partner1.dmg emefree.dmg"
-        ])
+        if 'mac' in task['attributes']['build_platform']:
+            task["run"]["command"] = " ".join([
+                "cd", "/builds/worker/checkouts/gecko", "&&",
+                "curl -L https://queue.taskcluster.net/v1/task/YfqwST9zRo2-QddTuetDQA/runs/0/artifacts/public/build/target.dmg > partner1.dmg && "
+                "cp partner1.dmg partner2.dmg && "
+                "cp partner1.dmg emefree.dmg"
+            ])
+        elif 'win32' in task['attributes']['build_platform']:
+            task["run"]["command"] = " ".join([
+                "cd", "/builds/worker/checkouts/gecko", "&&",
+                "curl -L https://queue.taskcluster.net/v1/task/aSYhH66QRDyYwhyjmt2wGQ/runs/0/artifacts/public/build/setup.exe > partner1.exe && "
+                "cp partner1.exe partner2.exe && "
+                "cp partner1.exe emefree.exe &&"
+                "curl -L https://queue.taskcluster.net/v1/task/aSYhH66QRDyYwhyjmt2wGQ/runs/0/artifacts/public/build/target.zip > partner1.zip && "
+                "cp partner1.zip partner2.zip && "
+                "cp partner1.zip emefree.zip"
+            ])
+        elif 'win64' in task['attributes']['build_platform']:
+            task["run"]["command"] = " ".join([
+                "cd", "/builds/worker/checkouts/gecko", "&&",
+                "curl -L https://queue.taskcluster.net/v1/task/QKLcYMFQTCiMWz1rlRlSoA/runs/0/artifacts/public/build/setup.exe > partner1.exe && "
+                "cp partner1.exe partner2.exe && "
+                "cp partner1.exe emefree.exe &&"
+                "curl -L https://queue.taskcluster.net/v1/task/QKLcYMFQTCiMWz1rlRlSoA/runs/0/artifacts/public/build/target.zip > partner1.zip && "
+                "cp partner1.zip partner2.zip && "
+                "cp partner1.zip emefree.zip"
+            ])
+        elif 'linux-' in task['attributes']['build_platform']:
+            task["run"]["command"] = " ".join([
+                "cd", "/builds/worker/checkouts/gecko", "&&",
+                "curl -L https://queue.taskcluster.net/v1/task/Q8txUznsRke5OQ-CVZuB1Q/runs/0/artifacts/public/build/target.tar.bz2 > target.tar.bz2 && "
+                "cp partner1.tar.bz2 partner2.tar.bz2 && "
+                "cp partner1.tar.bz2 emefree.tar.bz2 &&"
+            ])
+        elif 'linux64-' in task['attributes']['build_platform']:
+            task["run"]["command"] = " ".join([
+                "cd", "/builds/worker/checkouts/gecko", "&&",
+                "curl -L https://queue.taskcluster.net/v1/task/FkBd2xC_SSeBG4_ihZG5Zw/runs/0/artifacts/public/build/target.tar.bz2 > target.tar.bz2 && "
+                "cp partner1.tar.bz2 partner2.tar.bz2 && "
+                "cp partner1.tar.bz2 emefree.tar.bz2 &&"
+            ])
 
         yield task
