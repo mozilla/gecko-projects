0cbf4ad9960d27da6297d88446ea6485c84de26a
Ben Hearsum <bhearsum@mozilla.com>
2018-03-12 13:58:38 -0400

    Add missing files

---
+++ taskcluster/taskgraph/transforms/copy_build_platform_from_dependent_task.py
@@ -0,0 +1,23 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+Transform the repackage task into an actual task description.
+"""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import copy
+
+from taskgraph.transforms.base import TransformSequence
+
+transforms = TransformSequence()
+
+
+@transforms.add
+def copy_build_platform(config, jobs):
+    for job in jobs:
+        job.setdefault('attributes', {})
+        job['attributes']['build_platform'] = job['dependent-task'].attributes.get('build_platform')
+        job['attributes']['build_type'] = job['dependent-task'].attributes.get('build_type')
+        yield job
