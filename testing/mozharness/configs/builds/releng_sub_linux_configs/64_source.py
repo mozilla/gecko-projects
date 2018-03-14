a753edf7fdee674650e72163ed18b52736be6e6d
Aki Sasaki <asasaki@mozilla.com>
2018-03-13 17:57:20 -0700

    fix mozharness part 2

--- testing/mozharness/configs/builds/releng_sub_linux_configs/64_source.py
+++ testing/mozharness/configs/builds/releng_sub_linux_configs/64_source.py
@@ -1,11 +1,9 @@
 config = {
     'default_actions': [
         'clobber',
-        'clone-tools',
         'checkout-sources',
         'setup-mock',
         'package-source',
-        'generate-source-signing-manifest',
     ],
     'stage_platform': 'source',  # Not used, but required by the script
     'buildbot_json_path': 'buildprops.json',
