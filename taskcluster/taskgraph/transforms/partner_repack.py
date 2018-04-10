d277be6d3a3726ba8c5cc48b37cac704a85681e7
Aki Sasaki <asasaki@mozilla.com>
2018-04-10 11:41:28 -0700

    possibly fix partner repack upload
    --HG--
    extra : histedit_source : 029150a3909539fb50f3c49b6f2fac6174d6acac%2Cf92040edfd590b67a680a88b68c8ac710e8a9819

--- taskcluster/taskgraph/transforms/partner_repack.py
+++ taskcluster/taskgraph/transforms/partner_repack.py
@@ -10,8 +10,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 from taskgraph.transforms.base import TransformSequence
 from taskgraph.util.schema import resolve_keyed_by
 from taskgraph.util.scriptworker import get_release_config
-from taskgraph.util.partners import get_partner_config_by_kind, check_if_partners_enabled
-from taskgraph.util.taskcluster import get_artifact_path
+from taskgraph.util.partners import check_if_partners_enabled
 
 
 transforms = TransformSequence()
@@ -69,83 +68,14 @@ def add_command_arguments(config, tasks):
         yield task
 
 
-def generate_platform_artifacts(platform):
-    if platform.startswith('win'):
-        return ['target.zip', 'setup.exe']
-    elif platform.startswith('macosx'):
-        return ['target.tar.gz']
-    elif platform.startswith('linux'):
-        return ['target.tar.bz2']
-    else:
-        raise ValueError('Unimplemented platform %s'.format(platform))
-
-
-# seems likely this exists elsewhere already
-def get_ftp_platform(platform):
-    if platform.startswith('win32'):
-        return 'win32'
-    elif platform.startswith('win64'):
-        return 'win64'
-    elif platform.startswith('macosx'):
-        return 'mac'
-    elif platform.startswith('linux-'):
-        return 'linux-i686'
-    elif platform.startswith('linux64'):
-        return 'linux-x86_64'
-    else:
-        raise ValueError('Unimplemented platform %s'.format(platform))
-
-
-# TODO - can we generalise this for all partner tasks ?
 @transforms.add
 def add_artifacts(config, tasks):
     for task in tasks:
-        partner_configs = get_partner_config_by_kind(
-            config, config.kind
-        )
-        platform = task["attributes"]["build_platform"]
-        platform_files = generate_platform_artifacts(platform)
-
         task["worker"].setdefault("artifacts", [])
         task["worker"]["artifacts"].append({
-            'name': '/builds/worker/workspace/build/artifacts/releng/partner/',
+            'name': '/builds/worker/workspace/artifacts/releng/partner',
             'path': 'releng/partner',
             'type': 'directory',
         })
-
-#        if not task["worker"].get("artifacts"):
-#             task["worker"]["artifacts"] = []
-#        for partner, partner_config in partner_configs.iteritems():
-#            # TODO clean up configs? Some have a {} as the config
-#            for sub_partner, cfg in partner_config.iteritems():
-#                if not cfg or platform not in cfg.get("platforms", []):
-#                    continue
-#                for locale in cfg.get("locales", []):
-#                    # Some partner configs have public builds, and specific a path in the
-#                    # candidates directory
-#                    if cfg.get('upload_to_candidates') and cfg.get('output_dir'):
-#                        subst = {
-#                            'partner': partner,
-#                            'partner_distro': sub_partner,
-#                            'locale': locale,
-#                            'platform': get_ftp_platform(platform)
-#                        }
-#                        prefix = get_artifact_path(task,
-#                                                   cfg['output_dir'] % subst)
-#                    else:
-#                        prefix = get_artifact_path(task,
-#                                                   '{}/{}/{}'.format(partner, sub_partner, locale))
-#                    worker_prefix = '{}/{}'.format('/builds/worker/workspace/build/artifacts',
-#                                                   prefix)
-#
-#                    task["worker"]["artifacts"].extend(
-#                        [
-#                            {
-#                                'name': '{}/{}'.format(prefix, f),
-#                                'path': '{}/{}'.format(worker_prefix, f),
-#                                'type': 'file',
-#                            } for f in platform_files
-#                        ]
-#                    )
 
         yield task
