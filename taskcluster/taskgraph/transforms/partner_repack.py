79abeea5e9f6bccb1d69480e5eeaf3b3b1874b0d
Nick Thomas <nthomas@mozilla.com>
2018-04-18 12:48:05 +1200

    Code cleanup DONTBUILD
    --HG--
    extra : amend_source : 6b8e5739131a366c8c4e0a86ac2baebff465eeae

--- taskcluster/taskgraph/transforms/partner_repack.py
+++ taskcluster/taskgraph/transforms/partner_repack.py
@@ -15,10 +15,6 @@ from taskgraph.util.partners import check_if_partners_enabled
 
 transforms = TransformSequence()
 
-# debugging goop
-import logging
-log = logging.getLogger(__name__)
-
 
 transforms.add(check_if_partners_enabled)
 
