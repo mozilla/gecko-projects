a73df14c504efdaca2b03340061a1c8336661fe7
Mihai Tabara <mtabara@mozilla.com>
2018-05-02 14:37:44 +0300

    Bug 1446815 - fix comment in beetmover-release-source-checksums. DONTBUILD

--- taskcluster/taskgraph/transforms/beetmover_source_checksums.py
+++ taskcluster/taskgraph/transforms/beetmover_source_checksums.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
-Transform the checksums signing task into an actual task description.
+Transform beetmover-release-source-checksums task into an actual task description.
 """
 
 from __future__ import absolute_import, print_function, unicode_literals
