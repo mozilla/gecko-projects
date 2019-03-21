7c43e6b094875f473e3a930ecf1e097a7ebdf0c7
Anny Gakhokidze <agakhokidze@mozilla.com>
2019-03-21 16:33:51 -0400

    Bug 1536468 - Make retrieval of WebBrowserPersistDocumentAttrs.cacheKey async in SessionHistory, r=peterv
    Differential Revision: https://phabricator.services.mozilla.com/D24472

--- dom/webbrowserpersist/WebBrowserPersistDocumentChild.cpp
+++ dom/webbrowserpersist/WebBrowserPersistDocumentChild.cpp
@@ -13,6 +13,7 @@
 #include "WebBrowserPersistLocalDocument.h"
 #include "WebBrowserPersistResourcesChild.h"
 #include "WebBrowserPersistSerializeChild.h"
+#include "SHEntryChild.h"
 
 namespace mozilla {
 
@@ -55,7 +56,13 @@ void WebBrowserPersistDocumentChild::Start(
   ENSURE(aDocument->GetTitle(attrs.title()));
   ENSURE(aDocument->GetReferrer(attrs.referrer()));
   ENSURE(aDocument->GetContentDisposition(attrs.contentDisposition()));
-  ENSURE(aDocument->GetCacheKey(&(attrs.cacheKey())));
+
+  RefPtr<dom::SHEntryChild> shEntryChild =
+      aDocument->GetHistory().downcast<dom::SHEntryChild>();
+  if (shEntryChild) {
+    attrs.sessionHistoryEntryChild() = shEntryChild.get();
+  }
+
   ENSURE(aDocument->GetPersistFlags(&(attrs.persistFlags())));
 
   ENSURE(aDocument->GetPrincipal(getter_AddRefs(principal)));
