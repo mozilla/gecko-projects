0931b0643e898bee8c4c9b93e6f1926997e79775
Robert Strong <robert.bugzilla@gmail.com>
2019-06-05 09:29:09 -0700

    Backed out changeset 95688ae3bc54 DONTBUILD

--- toolkit/mozapps/update/tests/browser/browser_telemetry_updatePing_ready.js
+++ toolkit/mozapps/update/tests/browser/browser_telemetry_updatePing_ready.js
@@ -1,7 +1,6 @@
 /* Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/ */
-
-"use strict";
+   http://creativecommons.org/publicdomain/zero/1.0/
+*/
 
 ChromeUtils.import("resource://testing-common/TelemetryArchiveTesting.jsm", this);
 
